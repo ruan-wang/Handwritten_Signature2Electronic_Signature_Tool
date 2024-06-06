0b4f7b00e6f5ca1dc40a46d4aee9153139c1fe4e
ruan-wang <1614646426@qq.com>
2024-06-06 19:50:56 +0800

    提交信息描述

--- app.py
+++ app.py
@@ -68,7 +68,41 @@ def convert_signature_to_transparent_png(input_image):
 
 
 
+
+# 设置配色方案
+st.markdown(
+    """
+    <style>
+    .main {
+        background-color: #F3E5F5;
+        color: #8E24AA;
+    }
+    .stButton button {
+        background-color: #FF7043;
+        color:  #5D4037;
+    }
+    .stTitle {
+        color: #5D4037;
+    }
+    .stCaption {
+        color: #FFAB91;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+
 def main():
+    # 侧边栏个人介绍
+    st.sidebar.title("个人介绍")
+    profile_image = Image.open("Image/1.png")  # 替换为你的个人图片路径
+    st.sidebar.image(profile_image, use_column_width=True)
+    st.sidebar.write("""
+    你好，我是阮同学，目前在北京师范大学攻读博士。我平时喜欢编程捣鼓一些有趣的玩意儿。如果你有什么新奇的想法或者对我的作品有什么改进建议，欢迎告诉我！\n商务与学习交流：ruan_bilibili@163.com
+    """)
+
     st.title("手写签名转换为电子签名工具")
     uploaded_file = st.file_uploader("请选择一张手写签名图片", type=["jpg", "jpeg", "png"], help="支持的文件类型: jpg, jpeg, png. 最大文件大小: 10MB")
 
@@ -83,15 +117,19 @@ def main():
         buf = BytesIO()
         result_image.save(buf, format="PNG")
         byte_im = buf.getvalue()
-        st.download_button(
+        if st.download_button(
             label="下载电子签名",
             data=byte_im,
             file_name="电子签名.png",
             mime="image/png"
-        )
-
-
-
+        ):
+            # 下载按钮点击后显示介绍信息和图片
+            st.markdown("---")
+            money = Image.open("Image/2.png")  #
+            st.image(money, caption="关于我们", use_column_width=True)
+            st.write("""
+            谢谢你使用我的作品！如果觉得好用的话，看在UP这么无私奉献的份上，可否支持下UP呢？我会更加努力做出更好更实用的作品的！
+            """)
 
 if __name__ == "__main__":
     main()
@@ -99,4 +137,12 @@ if __name__ == "__main__":
 
 
 
-#python /nfs/home/1002_sunbo/RW_Experiments/Personal_project/Github_code/text.py
+    
+    
+    
+
+#ruan_bilibili@163.com
+#https://space.bilibili.com/76702965
+#
+
+#python /nfs/home/1002_sunbo/RW_Experiments/Personal_project/Github_Handwritten_Signature2Electronic_Signature_Tool/app.py
